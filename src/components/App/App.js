67e93593b79a1b1dce766a13faee6146eff8986f
kuroptew <kuroptevdb@gmail.com>
2023-01-22 18:56:30 +0300

    переместил компонент App в отдельную папку

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import useLocalStorage from "../../hooks/useLocalStorage"
+
+import FormUser from "../FormUser/FormUser"
+
+import "../../style/style.scss"
+
+const KEY_USER = "latestUser"
+
+function App() {
+  const [user, setUser] = useLocalStorage(KEY_USER, {})
+
+  return (
+    <div className="container">
+      <h1 className="title">Здравствуйте, <span>{user.name || "Человек"}</span></h1>
+      <FormUser user={user} setUser={setUser}/>
+    </div>
+  )
+}
+
+export default App
